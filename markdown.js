7c5134bd1fd97d1d2aaa3d9aa19c4852f1684482
Victor Pegado <victor@pegado.se>
2015-03-17 10:02:12 +0100

    Update markdown.js

--- markdown.js
+++ markdown.js
@@ -1,18 +1,19 @@
+'use strict';
+
 angular.module('markdown', [])
-    .provider('markdown', function () {
-        var opts = {};
-        return {
-            config: function (newOpts) {
-                opts = newOpts;
-            },
-            $get: function () {
-                return new Showdown.converter(opts);
-            }
-        };
-    })
-    .filter('markdown', ['$sce', 'markdown', function (markdown) {
-        return function (text) {
-            text = text || '';
-            return markdown.makeHtml(text);
-        };
-    }]);
+  .provider('markdown', [function () {
+    var opts = {};
+    return {
+      config: function (newOpts) {
+        opts = newOpts;
+      },
+      $get: function () {
+        return new window.Showdown.converter(opts);
+      }
+    };
+  }])
+  .filter('markdown', ['markdown', function (markdown) {
+    return function (text) {
+      return markdown.makeHtml(text || '');
+    };
+  }]);
